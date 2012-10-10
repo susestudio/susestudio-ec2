6f0747c82ba0cf3f6f4c00c3692f300d2142f7e2
James Tan <jatan@suse.de>
2012-10-10 13:51:20 +0200

    Add comments to script

--- create-openSUSE-AMIs.sh
+++ create-openSUSE-AMIs.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# Simple bash script for creating public openSUSE AMIs in all Amazon EC2
+# regions, from a Studio built openSUSE image.
+#
+# It assumes that the image tarball is in the form
+# 'Amazon_Machine_Image_AMI_12.1_32bit.i686-3.0.0.ec2.tar.gz', and parses the
+# filename to extract the openSUSE version (eg. 12.2), image version (eg.
+# 4.0.0) and architecture (i686 or x86_64).
+#
+# See https://github.com/susestudio/susestudio-ec2 for details and examples.
+#
+# Author: James Tan <jatan@suse.com>
 
 usage() {
   echo >&2 "Usage: create-openSUSE-AMIs TARBALL"
